9626afc22ae7ef769673236de2556c73c0ba98fd
Shivam Kumar Kashyap <109240141+MernWala@users.noreply.github.com>
2024-09-27 23:42:31 +0530

    mess: Configuration of react-bootstrap and bootstrap alone;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Toast from 'react-bootstrap/Toast';
 
 const App = () => {
+
+  const [show, setShow] = useState(false);
+  const toggleShow = () => setShow(!show);
+
   return (
-    <div className='flex flex-col gap-5 items-center'>
-      <div className='py-52'>
-        <h1 className='font-bold text-3xl mb-2'>Getting Start With <span className='text-blue-700'>React.js</span></h1>
+    <div className="d-flex flex-column align-items-center w-100">
+      <div className='pt-5 mt-5'>
+        <h1 className='font-weight-bold fs-2 mb-2'>Getting Start With <span className='text-primary'>React.js</span></h1>
         <p>Welcome to the journey of learning technical stuff</p>
       </div>
+
+      <Button onClick={toggleShow} className="mb-2">
+        Toggle Toast <strong>with</strong> Animation
+      </Button>
+
+      <Toast show={show} onClose={toggleShow}>
+        <Toast.Header>
+          <strong className="me-auto">Bootstrap</strong>
+          <small>11 mins ago</small>
+        </Toast.Header>
+        <Toast.Body>Woohoo, you're reading this text in a Toast!</Toast.Body>
+      </Toast>
+
     </div>
   )
 }
