eb5937fe3638e30a39fb27aece9c5ee51e77eb4a
Shivam Kumar Kashyap <109240141+MernWala@users.noreply.github.com>
2024-10-16 23:57:44 +0530

    Recaping BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import { Toaster } from 'react-hot-toast';
 import FindReplaceModal from './components/FindReplaceModal';
 import DataState from './context/data/DataState';
 import FunctionState from './context/functions/FunctionState';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About';
 
 const App = () => {
   return (
     <DataState>
       <FunctionState>
         <Toaster />
-        <Home />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+
+            {/* <Route path='/'>
+              <Route index element={<Home />} />
+              <Route path='/about' element={<About />} />
+            </Route> */}
+          </Routes>
+        </BrowserRouter>
         <FindReplaceModal />
       </FunctionState>
     </DataState>
